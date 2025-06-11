69c8097e06d5d02ec44d89410338c9df986c60a2
savitha <savithasuresh2410@gmail.com>
2025-06-11 21:22:49 +0800

    random faults per episode

--- src/run.py
+++ src/run.py
@@ -198,6 +198,8 @@ def run_sequential(args, logger):
 
     while runner.t_env <= args.t_max:
         # Run for a whole episode at a time
+        learner.mac.agent.init_random_fault()
+        learner.old_mac.agent.init_random_fault()
         episode_batch = runner.run(test_mode=False)
         buffer.insert_episode_batch(episode_batch)
 
