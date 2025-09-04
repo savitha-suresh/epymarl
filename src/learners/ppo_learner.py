e13ff2cd562e17a1ad04f97eb6dbbf5f9a8b4baa
savitha <savithasuresh2410@gmail.com>
2025-09-04 21:35:46 +0800

    no mask

--- src/learners/ppo_learner.py
+++ src/learners/ppo_learner.py
@@ -138,10 +138,10 @@ class PPOLearner:
                 ).sum()
                 / mask.sum()
             )
-            total_loss = pg_loss + 0.05 * aux_loss
+            #total_loss = pg_loss + 0.05 * aux_loss
             
             self.agent_optimiser.zero_grad()
-            total_loss.backward()
+            pg_loss.backward()
             grad_norm = th.nn.utils.clip_grad_norm_(
                 self.agent_params, self.args.grad_norm_clip
             )
