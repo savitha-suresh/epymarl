12ac8f918bbc8e5c86d3c9a5a0854c319997c6f9
savitha <savithasuresh2410@gmail.com>
2025-09-08 21:21:57 +0800

    count files

---
+++ soc_faulty_run_lbf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=savsrware-mappo
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=savitha@comp.nus.edu.sg
+#SBATCH --partition=long
+#SBATCH --time=05:00:00
+source ~/.bashrc
+conda activate ft-gym-env
+srun get_faulty_count_lbf.sh
