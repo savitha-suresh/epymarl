d3e5f2560c99dac8c0e5625e9f981f9454408758
savitha <savithasuresh2410@gmail.com>
2025-09-09 14:10:28 +0800

    lbf new env

--- src/main.py
+++ src/main.py
@@ -18,6 +18,8 @@ import torch as th
 
 from utils.logging import get_logger
 from run import run
+from envs.lbf_register import register_envs
+register_envs()
 
 SETTINGS["CAPTURE_MODE"] = (
     "fd"  # set to "no" if you want to see stdout/stderr in console
@@ -30,6 +32,7 @@ ex.captured_out_filter = apply_backspaces_and_linefeeds
 
 results_path = os.path.join(dirname(dirname(abspath(__file__))), "results")
 # results_path = "/home/ubuntu/data"
+
 
 
 @ex.main
