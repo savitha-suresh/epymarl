919bd2611aecdafae633d208edaf7a12b5a9b1b6
savitha <savithasuresh2410@gmail.com>
2025-09-16 09:51:35 +0800

    no fault

--- src/run.py
+++ src/run.py
@@ -87,7 +87,6 @@ def run(_run, _config, _log):
 
 def evaluate_sequential(args, runner):
     for _ in range(args.test_nepisode):
-        runner.mac.agent._faulty = False
         runner.run(test_mode=True)
 
     if args.save_replay:
@@ -230,7 +229,6 @@ def run_sequential(args, logger):
 
             last_test_T = runner.t_env
             for _ in range(n_test_runs):
-                runner.mac.agent._faulty = False
                 runner.run(test_mode=True)
 
         if args.save_model and (
@@ -260,10 +258,6 @@ def run_sequential(args, logger):
                     )
 
         episode += args.batch_size_run
-        if args.random_start:
-            if not args.action_fault:
-                learner.mac.agent._faulty = False
-                learner.old_mac.agent._faulty = False
 
         if (runner.t_env - last_log_T) >= args.log_interval:
             logger.log_stat("episode", episode, runner.t_env)
