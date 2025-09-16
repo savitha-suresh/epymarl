acb6a573c1e47d40baaa8fbcd1cbd34bfb248f69
savitha <savithasuresh2410@gmail.com>
2025-09-16 10:09:30 +0800

    rnn eval

--- src/modules/agents/faulty_agent.py
+++ src/modules/agents/faulty_agent.py
@@ -21,10 +21,12 @@ class FaultyAgent(RNNAgent):
         self.no_op_action = 0
     
     def init_random_fault(self):
-        self.faulty_agent_indices = {0}#set(random.sample(range(self.args.n_agents), 
-                                    #                  self.args.n_faulty_agents))
-        print(f"Agents {self.faulty_agent_indices} have become network faulty!")
         #self.faulty_agent_indices = {1}
+        # self.faulty_agent_indices = set(random.sample(range(self.args.n_agents), 
+        #                                               self.args.n_faulty_agents))
+        self.faulty_agent_indices = {int(self.args.fault_idx)}
+        print(f"Agents {self.faulty_agent_indices} have become network faulty!")
+        
         self._faulty = False
 
     
