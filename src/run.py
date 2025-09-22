08d9784da2129af37e4cbaf72363d6a4ad143c97
savitha <savithasuresh2410@gmail.com>
2025-09-22 13:41:23 +0800

    gauss random

--- src/run.py
+++ src/run.py
@@ -87,7 +87,7 @@ def run(_run, _config, _log):
 
 def evaluate_sequential(args, runner):
     for _ in range(args.test_nepisode):
-        runner.mac.agent._faulty = False
+        runner.mac.agent.reset_fault()
         runner.run(test_mode=True)
 
     if args.save_replay:
@@ -230,13 +230,13 @@ def run_sequential(args, logger):
 
             last_test_T = runner.t_env
             for _ in range(n_test_runs):
-                runner.mac.agent._faulty = False
+                runner.mac.agent.reset_fault()
                 runner.run(test_mode=True)
 
         if args.random_start:
             if not args.action_fault:
-                learner.mac.agent._faulty = False
-                learner.old_mac.agent._faulty = False
+                learner.mac.agent.reset_fault()
+                learner.old_mac.agent.reset_fault()
                 
         if args.save_model and (
             runner.t_env - model_save_time >= args.save_model_interval
