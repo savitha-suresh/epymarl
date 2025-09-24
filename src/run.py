19974178e84d2ef83e84b3abb1f5c60dabf2a4ac
savitha <savithasuresh2410@gmail.com>
2025-09-24 20:14:14 +0800

    random eval

--- src/run.py
+++ src/run.py
@@ -87,6 +87,7 @@ def run(_run, _config, _log):
 
 def evaluate_sequential(args, runner):
     for _ in range(args.test_nepisode):
+        runner.mac.agent._faulty = False
         runner.run(test_mode=True)
 
     if args.save_replay:
