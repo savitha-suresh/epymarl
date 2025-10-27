d44f68364bfa6f5ac842b3aebd6fb803a1f03614
savitha <savithasuresh2410@gmail.com>
2025-10-27 17:37:13 +0800

    bug fix

--- src/modules/agents/faulty_agent.py
+++ src/modules/agents/faulty_agent.py
@@ -23,7 +23,7 @@ class FaultyAgent(RNNAgent):
         
     
     def init_random_fault(self):
-        self.faulty_agent_indices = set(int(self.args.fault_idx))
+        self.faulty_agent_indices = set([int(self.args.fault_idx)])
 
         print(f"Agents {self.faulty_agent_indices} have become network faulty!")
         self._faulty = False
